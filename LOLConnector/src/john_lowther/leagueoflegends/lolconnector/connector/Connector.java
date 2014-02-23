0e94d3e7a6eeabba0cd349d450983bf4e0d652d3
John Lowther <jw.lowther@hotmail.co.uk>
2014-02-23 22:58:10 +0000

    KeyReader change and Connector object added

---
+++ LOLConnector/src/john_lowther/leagueoflegends/lolconnector/connector/Connector.java
@@ -0,0 +1,82 @@
+package john_lowther.leagueoflegends.lolconnector.connector;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import john_lowther.leagueoflegends.lolconnector.fileio.KeyReader;
+
+/**
+ * Provides general connection to the Riot API.
+ * @author John Lowther
+ */
+public class Connector {
+	private static Connector instance;
+	
+	/**
+	 * Gets the instance of the Connector object.
+	 * @return Connector
+	 */
+ 	public static Connector getInstance() {
+		if (instance != null) {
+			instance = new Connector();
+		}
+		
+		return instance;
+	}
+
+	/**
+	 * Passes the base url and gets the api_key itself.
+	 * @param baseUrl
+	 * @return response
+	 */
+	public String submitApiRequest(String baseUrl) {
+		return submitRequest(baseUrl, new KeyReader().getApiKey());
+	}
+	
+	/**
+	 * Passes in the api_key to the url.
+	 * @param baseUrl
+	 * @param api_key
+	 * @return response
+	 */
+	public String submitRequest(String baseUrl, String api_key) {
+		return submitRequest(baseUrl + "?api_key=" + api_key);
+	}
+	
+	/**
+	 * Main method that actually does the connection work.
+	 * @param urlString
+	 * @return response
+	 */
+	public String submitRequest(String urlString) {
+		HttpURLConnection connection = null;  
+
+		try {
+			URL url = new URL(urlString);
+			connection = (HttpURLConnection) url.openConnection();
+
+			BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			while ((inputLine = input.readLine()) != null) {
+				response.append(inputLine);
+			}
+
+			input.close();
+
+			return response.toString();
+		} catch (Exception e) {
+
+			e.printStackTrace();
+			return null;
+
+		} finally {
+			if(connection != null) {
+				connection.disconnect(); 
+			}
+		}
+	}
+}
--- LOLConnector/src/john_lowther/leagueoflegends/lolconnector/fileio/KeyReader.java
+++ LOLConnector/src/john_lowther/leagueoflegends/lolconnector/fileio/KeyReader.java
@@ -6,27 +6,20 @@ import java.io.FileReader;
 import java.io.IOException;
 
 /**
- * Contains a single method to retrieve the API key.
+ * Contains a single method to retrieve the API key. By default api_key should be in 
+ * a plain text file named "api_key.txt". The file should be in your classpath.
  * @author John Lowther
  */
 public class KeyReader {
-	
-	/**
-	 * Method that returns the api_key. api_key should be in a plain text file named "api_key.txt".
-	 * The file should be in your classpath.
-	 * @return api_key
-	 */
-	public String getApiKey() {
-		return getApiKey("api_key.txt");
-	}
+	private String api_keyLocation = "api_key.txt";
 	
 	/**
 	 * Method that returns the api_key, based on the file name.
 	 * @return api_key
 	 */
-	public String getApiKey(String filename) {
+	public String getApiKey() {
 	    try(
-	    	BufferedReader br = new BufferedReader(new FileReader(filename))) {
+	    	BufferedReader br = new BufferedReader(new FileReader(api_keyLocation))) {
 	        String line = br.readLine();
 	        return line;
 	        
@@ -38,5 +31,13 @@ public class KeyReader {
 		}
 	    
 		return null;
+	}
+
+	public String getApi_keyLocation() {
+		return api_keyLocation;
+	}
+
+	public void setApi_keyLocation(String api_keyLocation) {
+		this.api_keyLocation = api_keyLocation;
 	}
 }
