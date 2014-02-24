ede243b33d9e484eeaf8ad87c74d87cd1ebd9c43
John Lowther <jw.lowther@hotmail.co.uk>
2014-02-24 23:37:08 +0000

    Added getters and setters

--- LOLConnector/src/john_lowther/leagueoflegends/lolconnector/connector/ChampionConnector.java
+++ LOLConnector/src/john_lowther/leagueoflegends/lolconnector/connector/ChampionConnector.java
@@ -1,13 +1,22 @@
 package john_lowther.leagueoflegends.lolconnector.connector;
 
 import john_lowther.leagueoflegends.lolconnector.dataenums.Region;
+import john_lowther.leagueoflegends.lolconnector.dataenums.Version;
 
 /**
  * Provides champion requests.
+ * Supports BR, EUNE, EUW, LAN, LAS, NA, OCE.
  * @author John Lowther
  */
 public class ChampionConnector extends LOLConnector {
 	private final String allChampionRequest = "https://prod.api.pvp.net/api/lol/%s/%s/champion";
+	
+	public ChampionConnector() {
+		setSupportedRegions(Region.BRAZIL, Region.EU_NORDIC_AND_EAST, Region.EU_WEST,
+				Region.LATIN_AMERICA_NORTH, Region.LATIN_AMERICA_SOUTH,
+				Region.NORTH_AMERICA, Region.OCEANIA_AUSTRAILIA, Region.OCEANIA_NEW_ZEALAND);
+		setSupportedVersions(Version.ONE_POINT_ONE);
+	}
 	
 	/**
 	 * Default champion retrieval
--- LOLConnector/src/john_lowther/leagueoflegends/lolconnector/connector/LOLConnector.java
+++ LOLConnector/src/john_lowther/leagueoflegends/lolconnector/connector/LOLConnector.java
@@ -5,11 +5,19 @@ import john_lowther.leagueoflegends.lolconnector.dataenums.Version;
 
 /**
  * Has methods to help all connection classes. Also acts as a label.
+ * Connection classes should use the constructor format:
+ * <pre>
+ * <code>
+ * 		public ChampionConnector() {
+ *			setSupportedRegions(...);
+ *			setSupportedVersions(...);
+ *		}
+ *</code>
  * @author John Lowther
  */
 public abstract class LOLConnector {
-	private Region[] supportedRegions = Region.values();
-	protected Version[] supportedVersions = Version.values();
+	protected Region[] supportedRegions;
+	protected Version[] supportedVersions;
 
 	/**
 	 * Adds a parameter to the request.
@@ -19,5 +27,21 @@ public abstract class LOLConnector {
 	 */
 	protected String addParamToRequest(String request, String paramName, Object param) {
 		return request + (request.contains("?") ? "&" : "?") + paramName + "=" + param.toString();
+	}
+	
+	protected Region[] getSupportedRegions() {
+		return supportedRegions;
+	}
+	
+	protected void setSupportedRegions(Region... regions) {
+		supportedRegions = regions;
+	}
+	
+	protected Version[] getSupportedVersions() {
+		return supportedVersions;
+	}
+	
+	protected void setSupportedVersions(Version... versions) {
+		supportedVersions = versions;
 	}
 }
