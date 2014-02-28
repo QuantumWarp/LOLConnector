c3ff05a366fa11646f86b73ca20924f1b224bbf8
John Lowther <jw.lowther@hotmail.co.uk>
2014-02-28 01:10:14 +0000

    Parsers empty added

--- LOLConnector/src/john_lowther/leagueoflegends/lolconnector/LOLRetriever.java
+++ LOLConnector/src/john_lowther/leagueoflegends/lolconnector/LOLRetriever.java
@@ -1,5 +1,25 @@
 package john_lowther.leagueoflegends.lolconnector;
 
+import john_lowther.leagueoflegends.lolconnector.dataenums.Region;
+import john_lowther.leagueoflegends.lolconnector.parselayer.SummonerParser;
+
 public class LOLRetriever {
+	private Region defaultRegion;
+	private SummonerParser summonerRequest = new SummonerParser();
+
+	public LOLRetriever() {
+		
+	}
 	
+	public LOLRetriever(Region defaultRegion) {
+		this.defaultRegion = defaultRegion;
+	}
+	
+	public long getSummonerId(String name) {
+		return summonerRequest.getSummonerId(defaultRegion, name);
+	}
+	
+	public SummonerParser summonerRequest() {
+		return summonerRequest;
+	}
 }
---
+++ LOLConnector/src/john_lowther/leagueoflegends/lolconnector/parselayer/StatsParser.java
@@ -0,0 +1,5 @@
+package john_lowther.leagueoflegends.lolconnector.parselayer;
+
+public class StatsParser {
+
+}
